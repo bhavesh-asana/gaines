eb08ed89dce74ffc6df948ba60a75235b06b6af6
bhavesh-asana <asanabhavesh@gmail.com>
2024-09-06 11:48:14 -0400

    scanning all the files from directory

--- textflow/urls.py
+++ textflow/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("pymupdf/", views.scanner_pymupdf, name="scan_pymupdf")
+    path("pymupdf/", views.scanner_pymupdf, name="scan_pymupdf"),
+    path("folderscan/", views.scan_folder, name="scan_folder")
 ]
--- textflow/views.py
+++ textflow/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-from PyPDF2 import PdfReader
 import pymupdf
 
+import os
+import glob
+
+# Define the directory path where your files are stored
+directory_path = './textflow/files/'
+
+# Use glob to find all files in the directory (e.g., *.pdf for all PDF files)
+file_paths = glob.glob(os.path.join(directory_path, '*.pdf'))
+
+# Scanning all the files in a directory
+def scan_folder(request):
+  content = []
+  # Loop through each file and read its contents
+  for file_path in file_paths:
+    with pymupdf.open(file_path) as doc:
+      for page in doc:
+        text = page.get_text()
+        content.append(text)
+    print("Finished Reading : " + file_path)
+  return HttpResponse(content)
+
+# Scanning an individual file
 def scanner_pymupdf(request):
   doc = pymupdf.open("./textflow/files/ml-handson.pdf")
   content = []
@@ -11,5 +31,4 @@ def scanner_pymupdf(request):
     text = page.get_text()
     # print(type(text))
     content.append(text)
-  
   return HttpResponse(content)
